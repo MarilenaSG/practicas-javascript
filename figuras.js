b22af2e0231d1a639644f9bebc9a9b1cf7682b7d
Marilena Sanchez <maryjay17@gmail.com>
2022-05-31 00:51:00 +0100

    Terminados los datos de las figuras

--- figuras.js
+++ figuras.js
@@ -3,8 +3,12 @@ console.group("Cuadrado");
 const ladoCuadrado = 5;
 console.log("Los lados del cuadrado miden: " + ladoCuadrado + " centímetros");
 
-const perimetroCuadrado = ladoCuadrado * 4;
-console.log("El perímetro del cuadrado es: " + perimetroCuadrado + " centímetros");
+function perimetroCuadrado(lado) {
+    return lado * 4;
+}
+
+
+console.log("El perímetro del cuadrado es: " + perimetroCuadrado(8) + " centímetros");
 
 const areaCuadrado = ladoCuadrado * ladoCuadrado;
 console.log("El area del cuadrado es: " + areaCuadrado + " centímetros cuadrados");
@@ -29,9 +33,25 @@ console.groupEnd;
 // Código del círculo
 
 console.group("Círculo");
-const radioCirculo = 5;
+const radioCirculo = 4;
+console.log("El radio del círculo es " + radioCirculo + " cm");
+
 const diametroCirculo = radioCirculo * 2;
-const pi = 3.1416;
+console.log("El diámetro del círculo es " + diametroCirculo + " cm");
+
+// PI
+const PI = Math.PI;
+console.log("PI es " + PI);
+
+// Perímtro circunferencia
+const circunferencia = PI * diametroCirculo;
+console.log("La circunferencia mide " + circunferencia + " cm");
+
+// Área Circulo
+const areaCirculo = (radioCirculo * radioCirculo) * PI;
+console.log("El área del círculo es " + areaCirculo + " cm cuadrados");
+console.groupEnd;
+
 
 
 
