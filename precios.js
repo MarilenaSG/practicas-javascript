ee6f415a991ce5b50fca5d2d8cbd6447c16e9590
Marilena Sanchez <maryjay17@gmail.com>
2022-05-31 23:36:14 +0100

    Terminado con un style horrible

--- precios.js
+++ precios.js
@@ -13,3 +13,36 @@ function onclickPrecio(){
     const precioConDescuento = calcularDescuento(priceValue, descuentoValue);
     ResultadoPrecio.innerText = "El precio con descuento es " + precioConDescuento + "€";
 }
+
+const cupones = ["desc10", "desc25", "desc40"];
+
+function onclickCupon() {
+    const precio = document.getElementById("inputPrecio");
+    const priceValue = inputPrecio.value;
+    const cupon = document.getElementById("inputCupon");
+    const cuponValue = inputCupon.value;
+    let descuento;
+
+    if (!cupones.includes(cuponValue)) {
+        alert("¡El cupón " + cuponValue + " no es válido!")
+    }
+    
+    else if (cuponValue === cupones[0]) {
+       descuento = 10;
+    }
+
+    else if (cuponValue === cupones[1]) {
+       descuento = 25;
+    }
+
+    else if (cuponValue === cupones[2]) {
+       descuento = 40;
+    }
+    
+
+    const precioConDescuento = calcularDescuento(priceValue, descuento);
+    const resultadoPrecio = document.getElementById(ResultadoPrecio);
+    ResultadoPrecio.innerText = "El precio con descuento es " + precioConDescuento + "€";
+
+}
+
