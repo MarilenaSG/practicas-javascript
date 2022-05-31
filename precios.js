040949d2b8e5da56d8b9d6caff5bde68d6c9ce6e
Marilena Sanchez <maryjay17@gmail.com>
2022-05-31 22:05:54 +0100

    Terminado el html y el script, integrado para el calculo de descuento

--- precios.js
+++ precios.js
@@ -1,6 +1,15 @@
-function calcularDescuento() {
-    const precio = document.getElementById("inputPrecio");
-    const descuento = document.getElementById("inputDescuento");
-    const precioFinal = (precio * (100 - descuento)) / 100;
+function calcularDescuento(precio, descuento) {
+    
+    const precioConDescuento = 100 - descuento;
+    const precioFinal = (precio * precioConDescuento) / 100;
     return precioFinal;
+}
+
+function onclickPrecio(){
+    const precio = document.getElementById("inputPrecio");
+    const priceValue = inputPrecio.value;
+    const descuento = document.getElementById("inputDescuento");
+    const descuentoValue = inputDescuento.value;
+    const precioConDescuento = calcularDescuento(priceValue, descuentoValue);
+    ResultadoPrecio.innerText = "El precio con descuento es " + precioConDescuento + "€";
 }
